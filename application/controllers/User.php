8bec8e7f353c41dcbab7f50e829712e407f6d299
Erdi Bagus <erdibgs@gmail.com>
2023-01-04 09:25:37 +0700

    update user controller phpspreadsheet

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 class User extends CI_Controller {
 
 	public function __construct() {
